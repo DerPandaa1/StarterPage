da5b421c218d612b5654364fc3cbcdc5537a256d
robslam <robinschramm16@yahoo.com>
2024-02-01 23:53:53 +0100

    Mehr Änderungen

--- logic.js
+++ logic.js
@@ -1,25 +1,32 @@
 let websiteList = {
-    0:{"name": "Youtube"},
-    1:{"name": "Google"},
-    2:{"name": "Github"},
-    3:{"name": "Twitch"},
-    4:{"name": "Pornhub"},
+    0:{"name": "Youtube", "url":"https://www.youtube.com"},
+    1:{"name": "Google", "url":"https://www.google.de/"},
+    2:{"name": "Github", "url":"https://github.com/"},
+    3:{"name": "Twitch", "url":"https://www.twitch.tv/"},
+    4:{"name": "Pornhub", "url":"https://de.pornhub.com/"}
 };
+
 $(document).ready(function(){
 
-    $( ".input .searchBar" ).on( "keyup", function() {
-        let htmlCode = '<li><a class="website" href="https://www.youtube.com" style="text-decoration: none;"><img src="logos/youtube.png" class="logo" style="height: 50px;"><p class="website-link"><strong>Youtube</strong></p></a></li>';
+    $( ".searchBar" ).on( "keyup", function() {
+        //let htmlCode = '<li><a class="website" href="https://www.youtube.com"><img src="logos/youtube.png" class="logo"><p class="website-link"><strong>Youtube</strong></p></a></li>';
 
-        $('.website-list').append(htmlCode);
+        //$('.website-list').append(htmlCode);
 
 
 
         if($(this).val() !== ""){
+            $('.website-list li').remove();
             for (let i = 0; i < Object.keys(websiteList).length; i++){
                 if(websiteList[i]["name"].toLowerCase().includes($(this).val().toLowerCase())){
-                    console.log(websiteList[i]);
+                    console.log(websiteList[i]["url"]);
+                    let htmlCode = '<li><a class="website" href="'+websiteList[i]["url"]+'"><img src="logos/github.svg" class="logo"><p class="website-link"><strong>'+websiteList[i]["name"]+'</strong></p></a></li>';
+                    $('.website-list').append(htmlCode);
                 }
             }
+        }
+        else{
+            $('.website-list li').remove();
         }
     } );
 });
