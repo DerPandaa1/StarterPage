1b1c3f38f7e3718d1fc0257849421adba70ee704
robslam <robinschramm16@yahoo.com>
2024-02-08 23:28:09 +0100

    JSON-Bums mit JS funktioniert, jetzt nur noch logos

--- logic.js
+++ logic.js
@@ -1,29 +1,37 @@
-let websiteList = {
-    0:{"name": "Youtube", "url":"https://www.youtube.com"},
-    1:{"name": "Google", "url":"https://www.google.de/"},
-    2:{"name": "Github", "url":"https://github.com/"},
-    3:{"name": "Twitch", "url":"https://www.twitch.tv/"},
-    4:{"name": "Pornhub", "url":"https://de.pornhub.com/"}
-};
-
 $(document).ready(function(){
 
+    
+
     $( ".searchBar" ).on( "keyup", function() {
-        //let htmlCode = '<li><a class="website" href="https://www.youtube.com"><img src="logos/youtube.png" class="logo"><p class="website-link"><strong>Youtube</strong></p></a></li>';
-
-        //$('.website-list').append(htmlCode);
-
 
 
         if($(this).val() !== ""){
             $('.website-list li').remove();
+
+            var val = $(this).val();
+
+            !async function(){
+                let data = await fetch("websites.json")
+                    .then((response) => response.json())
+                    .then(data => {
+                        return data;
+                    })
+                    .catch(error => {
+                        console.error(error);
+                    });
+            
+
+            let websiteList = data.websites;
+            
             for (let i = 0; i < Object.keys(websiteList).length; i++){
-                if(websiteList[i]["name"].toLowerCase().includes($(this).val().toLowerCase())){
+                if(websiteList[i]["name"].toLowerCase().includes(val.toLowerCase())){
                     console.log(websiteList[i]["url"]);
                     let htmlCode = '<li><a class="website" href="'+websiteList[i]["url"]+'"><img src="logos/github.svg" class="logo"><p class="website-link"><strong>'+websiteList[i]["name"]+'</strong></p></a></li>';
                     $('.website-list').append(htmlCode);
                 }
             }
+
+            }();
         }
         else{
             $('.website-list li').remove();
