22bee29db00737deab243aed6ca28f02be215fdd
robslam <robinschramm16@yahoo.com>
2024-02-25 14:30:53 +0100

    Icon hinzufügen funktioniert: Dateityp ist dabei egal Website URLs sollten jetzt auch funktionieren Jetzt fehlt nur noch etwas Validierung und Design

--- app.js
+++ app.js
@@ -8,20 +8,76 @@ const express = require('express');
 
 const app = express();
 
+const path = require('path');
+
+const multer = require('multer');
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'public/assets/images/logos/');
+    },
+    filename: (req, file, cb) => {
+        cb(null, (file.originalname));
+    }
+});
+
+const upload = multer({storage: storage});
+
 app.use(express.static('public'));
+
+
+// File upload
+app.get("/upload", (req, res) => {
+    res.render("Upload file");
+});
+
+
+app.post("/upload", upload.single('websiteIcon'), (req, res) => {
+    let websiteName = req.body.websiteName;
+    let websiteURL = req.body.websiteURL;
+    let fileName = req.file.originalname;
+    if(!websiteURL.includes('http')){
+        websiteURL = 'https://' + websiteURL;
+    }
+    // Insert website in json file
+    var websites = jsonData;
+    var website = {name: websiteName, url: websiteURL, icon: fileName}
+
+    for (var i = 0; i < websites.websites.length; i++){
+        if(websites.websites[i].name === website.name){
+            res.status(200);
+            res.send('Website bereits vorhanden');
+            return;
+        }
+    }
+
+
+
+    websites.websites[websites.websites.length] = website;
+
+    websites = JSON.stringify(websites);
+
+
+    
+    fs.writeFileSync('websites.json', websites, 'utf8');
+    fs.close;
+    res.status(200);
+    res.redirect('index.html');
+});
+
+
 
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json());
 
 
-
+// Json Datei auslesen und client übergeben
 app.post('/readJson', (req, res) => {
     res.status(200);
     res.send(jsonData);
 });
 
 
-
+/*
 app.post('/addJson', (req, res) => {
     //res.send('test');
     var websites = jsonData;
@@ -50,7 +106,7 @@ app.post('/addJson', (req, res) => {
     res.status(200);
     res.send('Data added');
 });
-
+*/
 
 
 
--- public/js/logic.js
+++ public/js/logic.js
@@ -22,7 +22,7 @@ $(document).ready(function(){
             
                     for (let i = 0; i < Object.keys(websiteList).length; i++){
                         if(websiteList[i]["name"].toLowerCase().includes(val.toLowerCase())){
-                            let htmlCode = '<li><a class="website" href="'+websiteList[i]["url"]+'"><img src="assets/images/logos/'+websiteList[i]["icon"]+'" class="logo"><p class="website-link"><strong>'+websiteList[i]["name"]+'</strong></p></a></li>';
+                            let htmlCode = '<li><a class="website" target="_blank" href="'+websiteList[i]["url"]+'"><img src="assets/images/logos/'+websiteList[i]["icon"]+'" class="logo"><p class="website-link"><strong>'+websiteList[i]["name"]+'</strong></p></a></li>';
                             $('.website-list').append(htmlCode);
                         }
                     }
@@ -39,36 +39,5 @@ $(document).ready(function(){
 
     $('.close-btn').on("click", function (){
         $('.add-json-entry-outer').hide();
-    });
-
-    $('.add-json-entry-btn').on("click", function(){
-
-
-        //console.log('here');
-        
-        
-        var websiteName = $('#websiteName').val();
-        var websiteURL = $('#websiteURL').val();
-
-        var website = {name: websiteName, url:websiteURL, icon:"youtube.svg"}
-
-        // Daten hinzufügen funktioniert hiermit
-
-        fetch('/addJson', {
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-              },
-              method: "POST",
-              body: JSON.stringify({
-                website
-            })
-        })
-            .then((response) => response.text())
-            .then((body) => {
-                console.log(body);
-            });
-
-
     });
 });
